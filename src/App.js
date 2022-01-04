1a0d12d9e66f5ed4053d9b49831e9e809784288e
Lester Tan Wan Jern <lester@appleseeds.my>
2022-01-05 01:44:05 +0800

    Add custom window component using window api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './styles/App.scss';
 import "tailwindcss/tailwind.css"
 
+// import NewWindow from 'react-new-window'
+import NewWindow from './components/NewWindow';
+import { useState, useRef, useEffect } from 'react';
+
 function App() {
+
+  const [openWindow, setOpenWindow] = useState(false);
+  const [inc, setInc] = useState(0);
+
   return (
     <div className="">
       <p className="text-yellow-800">Testing</p>
+      <button onClick={()=>setOpenWindow(prev=>!prev)} className="px-4 py-2 bg-blue-300">test {openWindow ? 'true':'false'}</button>
+      <button onClick={()=>setInc(prev=>prev+1)} className="px-4 py-2 bg-blue-500">inc</button>
+
+      {openWindow && 
+        <NewWindow 
+          title={"test "+inc}
+          moveTo={[10*inc, 10*inc]}
+          onUnload={(bool)=>setOpenWindow(bool)}
+        >
+          Hi 👋 {inc}
+        </NewWindow>
+      }
     </div>
   );
 }
---
+++ src/components/NewWindow.js
@@ -0,0 +1,57 @@
+import { useState, useRef, useEffect } from 'react';
+import { createPortal } from "react-dom";
+
+const NewWindow = ({
+    children, 
+    title,
+    moveTo,
+    onUnload
+}) => {
+
+    const [container, setContainer] = useState(null);
+    const newWindow = useRef(window);
+
+    // https://github.com/rmariuzzo/react-new-window/blob/master/src/NewWindow.js
+    // https://thewebdev.info/2021/10/03/how-to-open-a-component-in-new-window-on-a-click-in-react/
+
+    useEffect(() => {
+        const div = document.createElement("div");
+        setContainer(div);
+    }, []);
+
+    useEffect(() => {
+        if (container) {
+            newWindow.current = window.open("", title, "width=600,height=400,left=0,top=0");
+            newWindow.current.document.body.appendChild(container);
+            newWindow.current.document.title = title;
+            const curWindow = newWindow.current;
+
+            const windowCheckerInterval = setInterval(() => {
+                if (!newWindow.current || newWindow.current.closed) {
+                    onUnload(false)
+                }else{
+                }
+            }, 50);
+
+            newWindow.current.addEventListener('beforeunload', () => {
+                onUnload(false);
+                clearInterval(windowCheckerInterval)
+            })
+
+            return () => curWindow.close();
+        }
+    }, [container]);
+
+    useEffect(()=>{
+        if(newWindow.current){
+            newWindow.current.moveTo(moveTo[0], moveTo[1])
+        }
+        // console.log(moveTo[0], moveTo[1]);
+    }, [moveTo])
+
+   
+    return container && createPortal(children, container);
+};
+
+
+export default NewWindow;
